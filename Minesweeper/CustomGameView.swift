e605a29ab79a77e5af43a5f6de310fa5326c11c3
Cameron Goddard <camerongoddard@me.com>
2025-07-05 01:45:30 -0400

    add new custom game sheet

---
+++ Minesweeper/CustomGameView.swift
@@ -0,0 +1,14 @@
+//
+//  CustomGameView.swift
+//  Minesweeper
+//
+//  Created by Cameron Goddard on 7/2/25.
+//
+
+import SwiftUI
+
+struct CustomGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
